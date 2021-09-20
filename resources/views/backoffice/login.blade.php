1fefa00b06288b5b5911a75d22b9aae800003eca
titit <archiecakra@kominfo.surabaya.go.id>
2021-09-20 10:05:17 +0700

    (MOD) Login function

--- resources/views/backoffice/login.blade.php
+++ resources/views/backoffice/login.blade.php
@@ -26,38 +26,54 @@
         <div class="text-center mb-4">
           <a href="."><img src="{{ asset('assets/images/logo.png') }}" height="50" alt=""></a>
         </div>
-        <form class="card card-md" action="." method="get" autocomplete="off">
+        <form class="card card-md" action="{{ route('login') }}" method="post" autocomplete="off">
+          @csrf
           <div class="card-body">
             <h2 class="card-title text-center mb-4">Backoffice Aplikasi</h2>
+
             <div class="mb-3">
               <label class="form-label">Username</label>
-              <input type="text" class="form-control" placeholder="Masukkan username">
+              <input type="text" name="username" class="form-control @error('username') is-invalid @enderror" value="{{ old('username') }}" placeholder="Masukkan username">
+              @error('username')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
             </div>
+
             <div class="mb-2">
               <label class="form-label">
-                Masukkan Password
+                Password
                 {{-- <span class="form-label-description">
                   <a href="./forgot-password.html">Lupa password</a>
                 </span> --}}
               </label>
-              <div class="input-group input-group-flat">
-                <input type="password" class="form-control"  placeholder="Password"  autocomplete="off">
+              <div class="input-group">
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Masukkan password" autocomplete="off">
                 <span class="input-group-text">
                   <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="2" /><path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" /></svg>
                   </a>
                 </span>
+                @error('password')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                @enderror
               </div>
             </div>
+
             <div class="mb-2">
               <label class="form-check">
                 <input type="checkbox" class="form-check-input"/>
                 <span class="form-check-label">Ingat saya di perangkat ini</span>
               </label>
             </div>
+
             <div class="form-footer">
               <button type="submit" class="btn btn-primary w-100">Sign in</button>
             </div>
+
           </div>
         </form>
         {{-- <div class="text-center text-muted mt-3">
